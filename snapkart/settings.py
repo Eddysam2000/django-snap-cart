2b8eefaa12c1c91e0ddd7bd07203f201a97fc393
Eddysam2000 <samueledwin67@gmail.com>
2023-10-29 23:06:33 +0100

    account activation

--- snapkart/settings.py
+++ snapkart/settings.py
@@ -140,6 +140,13 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
 
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
